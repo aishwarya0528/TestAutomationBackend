01353dd9d275ca85ea52b91d831c7deca29f014c
shreyagoyal-06 <shreya.goyal@peopletech.com>
2024-10-17 15:12:44 -0700

    Update LoginServletTest.java

--- backend/src/test/java/LoginServletTest.java
+++ backend/src/test/java/LoginServletTest.java
@@ -1,6 +1,4 @@
-Here are the JUnit test cases for the LoginServlet class:
 
-```java
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -145,4 +143,3 @@ public class LoginServletTest {
         verify(request).getSession();
     }
 }
-```
